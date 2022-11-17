74e600e7f7d5b94cc01efedb87de15ad11bf193b
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-11-17 17:00:07 -0300

    tratamento de erros com nullish operator

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -11,7 +11,7 @@
 //response.status(201).json({ name, email, password });
 
 //Usamos métodos dentro da classe, por isso não colocamos a palavra reservada function. No caso a diferença entre function e método é: Uma função pode ser chamada diretamente por seu nome. Um método consiste em um código que pode ser chamado pelo nome de seu objeto e seu nome de método usando a notação de ponto ou a notação de colchetes
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 const AppError = require('../utils/appError');
 const sqliteConnection = require('../database/sqlite');
 class UsersControllers {
@@ -36,7 +36,7 @@ class UsersControllers {
   }
 
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
@@ -48,17 +48,33 @@ class UsersControllers {
 
     const userWithUpdateEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
 
-    //Se o id do email que esta sendo cadastrado é diferente do id de um email já cadastrado siginifica que esse email já esta em  uso com outro usuário.
+    //Se o id do email que esta sendo cadastrado é diferente do id de um email já cadastrado significa que esse email já esta em  uso com outro usuário.
     if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
       throw new AppError('Oops, Este e-mail já está em uso!');
     }
 
-    user.name = name;
-    user.email = email;
+    //O operador de união nula (??) é um operador lógico que retorna seu operando do lado direito quando seu operando do lado esquerdo é null ou undefined, caso contrário, retorna seu operando do lado esquerdo.
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError('Oops, Você precisa informar a senha antiga para definir a nova senha!!');
+    }
+
+    if (password && old_password) {
+      //comparação se a senha antiga que esta cadastrada no banco bate com a senha informada pelo usuário
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError('Oops, A senha antiga não confere!!!');
+      }
+
+      user.password = await hash(password, 8);
+    }
 
     //UPDATE users SET => atualiza a tabela users e defina...
     //set => definir
-    await database.run('UPDATE users SET name = ?, email = ?, update_at = ? WHERE id = ?', [user.name, user.email, new Date(), id]);
+    await database.run('UPDATE users SET name = ?, email = ?, password = ?, update_at = DATETIME("now") WHERE id = ?', [user.name, user.email, user.password, id]);
 
     return response.status(200).json();
   }
