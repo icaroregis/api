a3cc27f27fb98a2f18303d6ff5434c26e7179eb6
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-11-17 00:06:06 -0300

    inserindo columns e values no bd

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -6,21 +6,31 @@
 //4. update => Put para atualizar um registro.
 //5. delete => Remover um registro.
 
+//response é a interface de resposta HTTP, você pode enviar cabeçalhos e o corpo para o navegador.
+//response.send(`Usuário: ${name} - E-mail: ${email} - senha: ${password}`);
+//response.status(201).json({ name, email, password });
+
 //Usamos métodos dentro da classe, por isso não colocamos a palavra reservada function. No caso a diferença entre function e método é: Uma função pode ser chamada diretamente por seu nome. Um método consiste em um código que pode ser chamado pelo nome de seu objeto e seu nome de método usando a notação de ponto ou a notação de colchetes
 
 const AppError = require('../utils/appError');
+const sqliteConnection = require('../database/sqlite');
 class UsersControllers {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError('Nome é obrigatório!');
+    const database = await sqliteConnection();
+    //a interrogação simboliza a substituição da interrogação pela variável "[email]".
+    //selecione todos os campos da tabela usuário onde o email seja igual ao email que o usuário esta tentando cadastrar.
+    const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
+
+    //se o email que o usuário esta tentando cadastrar for igual ao email cadastrado no banco de dados...
+    if (checkUserExists) {
+      throw new AppError('Oops, Este e-mail já está em uso!');
     }
 
-    // response é a interface de resposta HTTP, você pode enviar cabeçalhos e o corpo para o navegador.
-    // response.send(`Usuário: ${name} - E-mail: ${email} - senha: ${password}`);
+    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password]);
 
-    response.status(201).json({ name, email, password });
+    return response.status(201).json();
   }
 }
 
--- src/database/sqlite/migrations/createUsers.js
+++ src/database/sqlite/migrations/createUsers.js
@@ -1,8 +1,8 @@
-//IF NOT EXISTS => quando não existir. Para não dar conflito com a tabela que já existe, caso já exista alguma.
+//IF NOT EXISTS => quando não existir. Para não dar conflito com a tabela que já existe, caso já exista alguma criada.
 
 const createUsers = `
   CREATE TABLE IF NOT EXISTS users ( 
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
     name VARCHAR,
     email VARCHAR,
     password VARCHAR,
